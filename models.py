3f098c589ab38ef5697f5a4bfeffbf9cece9b896
rsanromanlorenzo <rsanro@hotmail.com>
2015-06-07 16:01:40 +0200

    Commit inicial Academy

---
+++ models.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+from openerp import models, fields, api
+
+
+class Teachers(models.Model):
+    _name = 'academy.teachers'
+
+    name = fields.Char()
+
+# class academy(models.Model):
+#     _name = 'academy.academy'
+
+#     name = fields.Char()
